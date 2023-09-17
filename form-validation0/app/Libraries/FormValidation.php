c44a6e2e81815e445564cf2b1dc9b65057dbd08d
Ratianarivo Ny Voara Hir' Ainatsara <ratianarivodylan@gmail.com>
2023-09-17 14:14:45 +0300

    FormValidation

---
+++ form-validation0/app/Libraries/FormValidation.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Libraries;
+
+use App\Controllers\BaseController;
+
+class FormValidation extends BaseController {
+
+
+    // need to be lowercased
+    protected array $allowedMethods = ['post', 'get'];
+
+
+    public function verification ($rules=null , $errors =null) 
+    {
+
+        if (!in_array($this->request->getMethod(false), $this->allowedMethods, true)) {
+
+            return false;
+
+        }
+
+        return $this->validate($rules,$errors);
+
+    }
+
+}
