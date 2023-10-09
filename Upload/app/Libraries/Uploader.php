94c800026c713f513f0dbb1a387c7ff9ecbc1be3
Ratianarivo Ny Voara Hir' Ainatsara <ratianarivodylan@gmail.com>
2023-10-09 18:41:26 +0300

    create

--- Upload/app/Libraries/Uploader.php
+++ Upload/app/Libraries/Uploader.php
@@ -6,7 +6,15 @@ use App\Controllers\BaseController;
 
 class Uploader extends BaseController
 {
-
     
+    public function upldoad($fileName)
+    {
+        $file = $this->request->getFile($fileName);
+        if (! $file->hasMoved()) 
+        {
+            $filepath = WRITEPATH . 'uploads/' . $file->store();
+
+        }
+    }
     
 }
