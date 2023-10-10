0c380bd94fb0b68d9709aac913a10eb22a41b1f3
Ratianarivo Ny Voara Hir' Ainatsara <ratianarivodylan@gmail.com>
2023-10-10 11:04:33 +0300

    SendingMail

---
+++ Upload/app/Libraries/EmailSender.php
@@ -0,0 +1,66 @@
+<?php 
+
+namespace App\Libraries;
+
+use CodeIgniter\Email\Email;
+
+class EmailSender 
+{
+
+    protected Email $email ;
+
+    public function __construct(?Email $email=null ,$conf=null)
+    {
+        if ($email==null) {
+            $email =\Config\Services::email();
+        }
+        $this->email = $email;
+
+        $this->configSend($conf);
+    }
+
+
+    public function configSend($conf = ['protocol'=>'sendmail',
+                                        'mailPath'=>'/usr/sbin/sendmail',
+                                        'charset'=>'iso-8859-1',
+                                        'wordWrap'=>true
+                                        ])
+    {
+        $this->email->initialize($conf);
+    }
+    /**
+     * Mila mi set anle fandehany alony 
+     * form anle array
+     */
+    public function sendEmail()
+    {
+
+    }
+
+
+    public function setFrom(array $array)
+    {
+        $fromMail = isset($array['fromMail']) ? $array['fromMail'] : '';
+        
+        $yourName = isset($array['yourName']) ? $array['yourName'] : '';
+        
+        $toMail = isset($array['toMail']) ? $array['toMail'] : '';
+        
+        $CC = isset($array['CC']) ? $array['CC'] : '';
+        
+        $BCC = isset($array['BCC']) ? $array['BCC'] : '';
+        
+        $subject = isset($array['subject']) ? $array['subject'] : '';
+        
+        $message = isset($array['message']) ? $array['message'] : '';
+    
+        $this->email->setFrom($fromMail, $yourName)
+            ->setTo($toMail)
+            ->setCC($CC)
+            ->setBCC($BCC)
+            ->setSubject($subject)
+            ->setMessage($message)
+            ->send();
+    }
+    
+}
