dfade31f6fd09d17b6f01dfce82e6eb336bdb306
Ratianarivo Ny Voara Hir' Ainatsara <ratianarivodylan@gmail.com>
2023-10-10 08:40:32 +0300

    deleted:    FormValidation.md 	new file:   Libraries/.gitkeep 	modified:   Libraries/Uploader.php

--- Libraries/Uploader.php
+++ Libraries/Uploader.php
@@ -5,6 +5,7 @@ namespace App\Libraries;
 use App\Controllers\BaseController;
 use CodeIgniter\HTTP\CLIRequest;
 use CodeIgniter\HTTP\IncomingRequest;
+use Exception;
 
 class Uploader extends BaseController
 {
@@ -13,9 +14,10 @@ class Uploader extends BaseController
         
     }
 
-    public function upload(CLIRequest|IncomingRequest $request,string $fileName)
+    public function uploadSingle(CLIRequest|IncomingRequest $request,string $fileName)
     {
         $file = $request->getFile($fileName);
+
         if (! $file->hasMoved()) 
         {
             $filepath = WRITEPATH . 'uploads/' . $file->store();
@@ -23,6 +25,53 @@ class Uploader extends BaseController
             return $filepath;
         }
         return null;
+    }
+
+    public function uploadGroupFile(CLIRequest|IncomingRequest $request, array $fileNames)
+    {
+        $result =array();
+        foreach ($fileNames as $fileName) 
+        {
+            try 
+            {
+                $result[]= $this->uploadSingle($request , $fileName);
+            } 
+            catch (\Throwable $th) 
+            {
+                $file = $request->getFiles($fileName);
+
+                array_merge($result ,$this->arrayFilesTreatement($request,$file)) ;
+
+            }
+        }
+
+    }
+
+    private function arrayFilesTreatement(CLIRequest|IncomingRequest $request, array $files)
+    {
+        $result =array() ;
+        foreach ($files as $file) 
+        {
+            if (is_array($file)) 
+            {
+                array_merge($result,$this->arrayFilesTreatement($request,$file));
+            }
+            else
+            {
+                if (! $file->hasMoved()) 
+                {
+                    $filepath = WRITEPATH . 'uploads/' . $file->store();
+        
+                    $result [] = $filepath;
+                }
+                else
+                {
+                    throw new Exception ("ERROR");
+                }
+            }
+        }
+        return $result ;
+
     }
     
 }
